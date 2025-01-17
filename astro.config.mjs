277d1547a9620f6431fc7deff2011e5f39bb1007
arsenii_klinichev <arsenklinichev@gmail.com>
2025-01-17 19:39:02 +0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://encyklop.com',
+	integrations: [tailwind(), react(), mdx(), sitemap()],
+	redirects: {
+		'/games': '/games/time-zones'
+	}
+});
